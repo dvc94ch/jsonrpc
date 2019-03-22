9061001eec4a79a6c0a4b077ef07eb232268d5e5
David Craven <david@craven.ch>
2019-03-22 07:40:33 +0100

    Add example.

--- derive/examples/std.rs
+++ derive/examples/std.rs
@@ -5,32 +5,32 @@ use jsonrpc_derive::rpc;
 #[rpc]
 pub trait Rpc {
 	/// Returns a protocol version
-	#[rpc(name = "protocolVersion")]
+	#[rpc(name = "protocolVersion", returns = "String")]
 	fn protocol_version(&self) -> Result<String>;
 
 	/// Adds two numbers and returns a result
-	#[rpc(name = "add", alias("callAsyncMetaAlias"))]
-	fn add(&self, _: u64, _: u64) -> Result<u64>;
+	#[rpc(name = "add", alias("callAsyncMetaAlias"), returns = "u64")]
+	fn add(&self, a: u64, b: u64) -> Result<u64>;
 
 	/// Performs asynchronous operation
-	#[rpc(name = "callAsync")]
-	fn call(&self, _: u64) -> FutureResult<String, Error>;
+	#[rpc(name = "callAsync", returns = "String")]
+	fn call(&self, a: u64) -> FutureResult<String, Error>;
 }
 
 struct RpcImpl;
 
 impl Rpc for RpcImpl {
-	fn protocol_version(&self) -> Result<String> {
-		Ok("version1".into())
-	}
+    fn protocol_version(&self) -> Result<String> {
+		    Ok("version1".into())
+	  }
 
-	fn add(&self, a: u64, b: u64) -> Result<u64> {
-		Ok(a + b)
-	}
+	  fn add(&self, a: u64, b: u64) -> Result<u64> {
+		    Ok(a + b)
+	  }
 
-	fn call(&self, _: u64) -> FutureResult<String, Error> {
-		future::ok("OK".to_owned())
-	}
+	  fn call(&self, _: u64) -> FutureResult<String, Error> {
+		    future::ok("OK".to_owned())
+    }
 }
 
 fn main() {
